928cffd8d2f03c0ddd65acc450e1d9c6121d4fbf
Omar Faruq <omarfaruq9723@gmail.com>
2025-05-03 20:38:16 +0600

    generate-prescription is initial as demo purpose
    In ts file have to create empty constructor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { CreateDoctorPatientComponent } from './page/admin/create-doctor-patient/create-doctor-patient.component';
 import { AddMedicineComponent } from './add-medicine/add-medicine.component';
+import { GeneratePrescriptionComponent } from './generate-prescription/generate-prescription.component';
 
 export const routes: Routes = [
     
-    {path: 'add-medicine', component: AddMedicineComponent},
+    {path: 'add-medicine', component: AddMedicineComponent},    
+    {path: 'generate-prescription', component: GeneratePrescriptionComponent },
     {path: 'create-doctor-patient', component: CreateDoctorPatientComponent},
     {path: '**', redirectTo: 'view-doctor'},
 
--- src/app/generate-prescription/generate-prescription.component.ts
+++ src/app/generate-prescription/generate-prescription.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-generate-prescription',
-  imports: [],
   templateUrl: './generate-prescription.component.html',
-  styleUrl: './generate-prescription.component.css'
+  styleUrls: ['./generate-prescription.component.css']
 })
 export class GeneratePrescriptionComponent {
+  prescriptionId: string = '123456';
+  patientName: string = 'John Doe';
+  doctorId: string = 'D78910';
+  medicineName: string = 'Napa500';
+  dosage: string = 'Morning before meal';
+  duration: string = '10 days';
+  investigation1: string = 'CBC';
+  investigation2: string = 'USG of PP';
+  advice: string = 'Walking 30 minutes daily';
+  followUpDate: string = '30/10/25';
 
+  constructor() { }
+
+  ngOnInit(): void { }
 }

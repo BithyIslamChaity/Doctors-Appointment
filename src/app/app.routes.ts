e234591cc7747f1bc6f54f1c973d8a6ef0ed6044
Omar Faruq <omarfaruq9723@gmail.com>
2025-05-03 13:50:33 +0600

    add-medicine initial
    Now generate-prescription will init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { CreateDoctorPatientComponent } from './page/admin/create-doctor-patient/create-doctor-patient.component';
+import { AddMedicineComponent } from './add-medicine/add-medicine.component';
 
 export const routes: Routes = [
     
-
+    {path: 'add-medicine', component: AddMedicineComponent},
     {path: 'create-doctor-patient', component: CreateDoctorPatientComponent},
     {path: '**', redirectTo: 'view-doctor'},
 
---
+++ src/app/generate-prescription/generate-prescription.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-prescription',
+  imports: [],
+  templateUrl: './generate-prescription.component.html',
+  styleUrl: './generate-prescription.component.css'
+})
+export class GeneratePrescriptionComponent {
+
+}

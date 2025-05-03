3573b5b5eaf83a4a5d8470c5432f5da4d81cbf0f
Shamaratun <shamaratunmeme@gmail.com>
2025-05-03 20:56:10 +0600

    view of specialist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 import { CreateDoctorPatientComponent } from './page/admin/create-doctor-patient/create-doctor-patient.component';
 import { AddMedicineComponent } from './add-medicine/add-medicine.component';
 import { GeneratePrescriptionComponent } from './generate-prescription/generate-prescription.component';
+import { ListDoctorComponent } from './page/list-doctor/list-doctor.component';
 
 export const routes: Routes = [
     
     {path: 'add-medicine', component: AddMedicineComponent},    
     {path: 'generate-prescription', component: GeneratePrescriptionComponent },
     {path: 'create-doctor-patient', component: CreateDoctorPatientComponent},
+    {path: 'list-doctor', component: ListDoctorComponent},
     {path: '**', redirectTo: 'view-doctor'},
 
 ];

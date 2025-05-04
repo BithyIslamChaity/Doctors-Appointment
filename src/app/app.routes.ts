011c57fb73fef2ebe704668cd537ddf05c9306a5
nahidzhassan <nahidhassann0007@gmail.com>
2025-05-04 09:05:03 +0600

    about and contact component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { AddDoctorComponent } from './page/add-doctor/add-doctor.component';
 import { ViewDoctorComponent } from './page/view-doctor/view-doctor.component';
 import { BookAppoinmentComponent } from './page/book-appoinment/book-appoinment.component';
 import { AppoinmentHistoryComponent } from './page/appoinment-history/appoinment-history.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { AboutComponent } from './page/about/about.component';
 
 export const routes: Routes = [
     
@@ -10,6 +12,8 @@ export const routes: Routes = [
     {path: 'view-doctor', component: ViewDoctorComponent},
     {path: 'book-appoinment', component: BookAppoinmentComponent},
     {path: 'appoinment-history', component: AppoinmentHistoryComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutComponent},
     {path: '**', redirectTo: 'view-doctor'},
 
 ];

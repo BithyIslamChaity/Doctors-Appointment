323c4ddedec621399b02ff9c319b9703114f6a46
Shamaratun <shamaratunmeme@gmail.com>
2025-05-14 23:02:05 +0600

    go to bithy apu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { AddDoctorComponent } from './page/admin/add-doctor/add-doctor.component
 import { ListDoctorComponent } from './page/list-doctor/list-doctor.component';
 import { DoctorListComponent } from './page/doctor-list/doctor-list.component';
 import { PatientHistoryComponent } from './page/patient-history/patient-history.component';
-import { AddMedicineComponent } from './page/about/add-medicine/add-medicine.component';
+
 import { LoginComponent } from './page/login/login.component';
 import { PrivateComponent } from './page/private/private.component';
 import { authGuard } from './core/auth.guard';
 import { RegistrationComponent } from './page/registration/registration.component';
 import { AddPatientComponent } from './page/admin/add-patient/add-patient.component';
-import { A } from '@angular/cdk/keycodes';
+import { A, B } from '@angular/cdk/keycodes';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AddMedicineComponent } from './page/add-medicine/add-medicine.component';
+import { BookAppoinmentComponent } from './page/book-appoinment/book-appoinment.component';
 
 export const routes: Routes = [
     // { path: '', component: LoginComponent, },
@@ -33,6 +35,7 @@ export const routes: Routes = [
             { path: 'add-patient', component: AddPatientComponent },
             { path: 'patient-history', component: PatientHistoryComponent },
             { path: 'doctor-list', component: DoctorListComponent },
+            { path: 'book-appoinment', component: BookAppoinmentComponent },
             { path: '**', redirectTo: 'home' },
         ]
         // , },
